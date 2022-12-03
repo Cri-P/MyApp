dfe1e2c803846d658875409d4d14b3e9b45cd0a2
Cri <staarryyniight@gmail.com>
2022-12-03 15:57:23 +0000

    initial layout and code

---
+++ scr/components/MyButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MyButton(props) {
+    return (
+        <View>
+            <Button>FirstButton</Button>
+        </View>
+
+    );
+}
+
+export default MyButton;
---
+++ scr/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomeScreen(props) {
+    return (
+        <View>
+            <Text> This is the Home Page</Text>
+        </View>
+
+    );
+}
+
+export default HomeScreen;
